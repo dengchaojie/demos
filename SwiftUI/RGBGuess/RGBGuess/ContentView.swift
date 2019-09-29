723fbddc08ce7d95559a718ab887ad7410d68d5d
dengchaojie <dengchaojiedcj@163.com>
2019-09-29 17:35:18 +0800

    new

--- SwiftUI/RGBGuess/RGBGuess/ContentView.swift
+++ SwiftUI/RGBGuess/RGBGuess/ContentView.swift
@@ -34,22 +34,55 @@ struct ContentView: View {
                         Text("B: \(Int(bGuess*255.0))")
                     }
                 }
-            }
+                }
+//            .padding(.horizontal,0).padding(.top, 0)
+            
             Button(action: {
                 self.isShowAlert = true
             }, label: {
                 Text("Hit Me")
             }).alert(isPresented: $isShowAlert) { () -> Alert in
-                Alert(title: Text("Your score"), message: Text("kdkdkdkd"))
+                Alert(title: Text("Your score"), message: Text("\(computeScore())"))
+            }
+            
+            VStack {
+                ColorSlider(value: $rGuess, textColor: .red)
+                ColorSlider(value: $gGuess, textColor: .green)
+                ColorSlider(value: $bGuess, textColor: .blue)
+
             }
         }
+//        .edgesIgnoringSafeArea(.all)
         
     }
-    
+    func computeScore() -> Int {
+        let rDiff = rTarget - rGuess
+        let gDiff = gTarget - gGuess
+        let bDiff = bTarget - bGuess
+        let diff = sqrt(rDiff*rDiff + gDiff*gDiff + bDiff*bDiff)
+        return Int((1.0 - diff) * 100 + 0.5)
+    }
 //    func buttonAction() -> Void {
 //
 //    }
+    
+    
 }
+
+struct ColorSlider: View {
+    @Binding var value: Double
+    var textColor: Color
+    var body: some View {
+        HStack {
+            Text("0").foregroundColor(textColor)
+            Slider(value: $value)
+            Text("255").foregroundColor(textColor)
+
+        }.padding()
+    }
+    
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
