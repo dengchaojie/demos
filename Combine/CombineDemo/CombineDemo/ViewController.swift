6f919d4ee695ede7177db9b4b9e9467c8f6b060d
dengchaojie <dengchaojiedcj@163.com>
2019-10-11 17:58:27 +0800

    new

---
+++ Combine/CombineDemo/CombineDemo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CombineDemo
+//
+//  Created by hrt03 on 2019/10/9.
+//  Copyright © 2019 dengchaojie. All rights reserved.
+//
+
+import UIKit
+import Combine
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+
+        
+    }
+
+
+}
+
---
+++ Combine/DemoCombine.playground/Pages/Publishers & Subscribers.xcplaygroundpage/Contents.swift
@@ -0,0 +1,83 @@
+import Combine
+import Foundation
+
+//参考https://www.jianshu.com/p/df8535b40079 https://github.com/AvdLee/CombineSwiftPlayground
+
+let publ = Just(28)
+let cancelHrt = publ.sink(receiveValue: { value in
+    
+    print(value)
+})
+cancelHrt.cancel()
+
+let cancelHrt2 = publ.sink(receiveCompletion: {
+    print("receiveCompletion: \($0)")
+
+}, receiveValue: {
+    print("receiveValue: \($0)")
+
+})
+cancelHrt2.cancel()
+
+//class Person {
+//    var name: String
+//    init(name: String) {
+//        self.name = name
+//    }
+//}
+//
+//let per = Person.init(name: "dcj")
+
+//Just and Subscribers.Sink
+//let once: Just = Just(100)
+//let observer: Subscribers.Sink<Int,Never> = Subscribers.Sink(receiveCompletion: {
+//    print("completed: \($0)")
+//}, receiveValue: {
+//    print("received value: \($0)")
+//})
+//once.subscribe(observer)
+
+//Subscribers.Assign and PassthroughSubject
+//class Student {
+//    let name: String
+//    var score: Int
+//
+//    init(name:String, score:Int) {
+//        self.name = name
+//        self.score = score
+//    }
+//}
+//
+//let stu = Student.init(name: "dcj", score: 90)
+//print(stu.score)
+//let obs = Subscribers.Assign(object: stu, keyPath: \.score)
+//let publisher = PassthroughSubject<Int, Never>()
+//publisher.subscribe(obs)
+//publisher.send(38)
+//print(stu.score)
+//publisher.send(100)
+//print(stu.score)
+
+/// CurrentValueSubject
+//let currentValue = CurrentValueSubject<Int, NSError>(1)
+//currentValue.sink(receiveCompletion: {
+//    print("11\($0)")
+//}, receiveValue: {
+//    print("22\($0)")
+//})
+//
+//currentValue.value = 2
+//currentValue.value = 3
+//currentValue.send(4)
+//currentValue.send(completion: Subscribers.Completion<NSError>.finished)
+//currentValue.send(completion: Subscribers.Completion<NSError>.failure(
+//    NSError.init(domain: "xxx.com", code: 500, userInfo: ["errorMsg":"error"])))
+//currentValue.value = 5
+
+
+///AnyPublisher、AnySubscriber、AnySubject
+//let pub = Publishers.Sequence<[String], Never>(sequence: ["1","2"]).eraseToAnyPublisher()
+
+
+
+
