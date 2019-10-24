30ab6bda375026b4735c4e284a57d71c74a879a0
dengchaojie <dengchaojiedcj@163.com>
2019-10-24 20:22:31 +0800

    new

---
+++ Lib/MoyaDemo/MoyaDemo/Package.swift
@@ -0,0 +1,13 @@
+//
+//  Package.swift
+//  MoyaDemo
+//
+//  Created by hrt03 on 2019/10/24.
+//  Copyright © 2019 dengchaojie. All rights reserved.
+//
+
+import Foundation
+
+
+
+.package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "13.0.0"))
