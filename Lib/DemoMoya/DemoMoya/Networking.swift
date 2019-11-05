6fa8ec597d17b68d19bdaf0f89db7582262bce26
dengchaojie <dengchaojiedcj@163.com>
2019-11-05 08:10:35 +0800

    new

--- Lib/DemoMoya/DemoMoya/Networking.swift
+++ Lib/DemoMoya/DemoMoya/Networking.swift
@@ -35,6 +35,9 @@ struct Networking {
         provider.request(target) { (result) in
             switch result {
             case . success(let response):
+//                if let str = try? response.mapString() {
+//                    print(str)
+//                }
                 if let json = try? response.mapJSON() as? [String: Any] {
                     successCallback(json)
                 }else {
