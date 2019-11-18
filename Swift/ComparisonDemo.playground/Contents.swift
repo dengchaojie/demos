9be2539e5738ff92fa42bdf87c23c958d17cb90b
dengchaojie <dengchaojiedcj@163.com>
2019-11-18 08:02:51 +0800

    new

--- Swift/ComparisonDemo.playground/Contents.swift
+++ Swift/ComparisonDemo.playground/Contents.swift
@@ -1,16 +1,48 @@
 import UIKit
+// map
+//let numbers: [Int] = [1,2,3,4]
+//
+//let numbersSquared = numbers.map { $0 * $0 }
+////    numbers.map { (num) -> Int in
+////    num * num
+////}
+//
+//print(numbersSquared)
+//
+//
+//let players = ["Kek","dIi","pwP"]
+//let lower = players.map { $0.lowercased() }
+//print(lower)
+// compactMap
+//let numbersStrings = ["1","2", "3", "four"]
+//let numberArray = numbersStrings.compactMap { Int($0)}
+//print(numberArray)
+//
+//let numberArrayOption = numbersStrings.map { Int($0)}
+//print(numberArrayOption)
+// flatMap
+//let eachNumber3Times = numbers.flatMap { Array.init(repeating: $0, count: 3)}
+//print(eachNumber3Times)
+//let eachNumber3TimesArray = numbers.map { Array.init(repeating: $0, count: 3)}
+//print(eachNumber3TimesArray)
 
-let numbers: [Int] = [1,2,3,4]
+// filter
+//let numbers = [1,2,3,4,5,6,7]
+//let newNumbers = numbers.filter { $0 < 5 }
+//print(newNumbers)
+//
+//let words = ["pineapple", "grapple", "banana", "applet"]
+//let newWords = words.filter { $0.contains("apple") }
+//print(newWords)
 
-let numbersSquared = numbers.map { $0 * $0 }
-//    numbers.map { (num) -> Int in
-//    num * num
-//}
+// reduce
+let numberReduce: [Int] = [1,2,3,4]
+let numberSum = numberReduce.reduce(11) { (sum, item) -> Int in
+    return sum + item
+}
+print(numberSum)
 
-print(numbersSquared)
 
 
-let players = ["Kek","dIi","pwP"]
-let lower = players.map { $0.lowercased() }
-print(lower)
+
 
---
+++ Swift/ComparisonDemo.playground/Sources/Filter.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+// filter
+let numbers = [1,2,3,4,5,6]
+let newNumbers = numbers.filter { $0 < 5 }
+//print(newNumbers)
