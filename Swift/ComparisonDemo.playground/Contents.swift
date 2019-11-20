86993329702c40830ada1dc4e5770994163c9b43
dengchaojie <dengchaojiedcj@163.com>
2019-11-20 20:33:49 +0800

    new

--- Swift/ComparisonDemo.playground/Contents.swift
+++ Swift/ComparisonDemo.playground/Contents.swift
@@ -35,25 +35,85 @@ import UIKit
 //let newWords = words.filter { $0.contains("apple") }
 //print(newWords)
 
-// reduce
-let numberReduce: [Int] = [1,2,3,4]
-let numberSum = numberReduce.reduce(11, +)
-//    numberReduce.reduce(11) { (sum, item) -> Int in
-//    return sum + item
+///// reduce
+//let numberReduce: [Int] = [1,2,3,4]
+//let numberSum = numberReduce.reduce(11, +)
+////    numberReduce.reduce(11) { (sum, item) -> Int in
+////    return sum + item
+////}
+//print(numberSum)
+//
+//let numberProduct = numberReduce.reduce(1, *)
+////    numberReduce.reduce(1) {
+////    $0 * $1
+////}
+////    numberReduce.reduce(1) { (result, item) -> Int in
+////    return result * item
+////}
+//
+//print(numberProduct)
+
+/// sorted
+//let numbers = [4, 200, 75, 0]
+//let numbersAscending = numbers.sorted(by: <)
+//let numbersDescending = numbers.sorted(by: >)
+//print(numbersAscending)
+//print(numbersDescending)
+//
+//struct Player {
+//    let name: String
+//    let score: Int
 //}
-print(numberSum)
-
-let numberProduct = numberReduce.reduce(1, *)
-//    numberReduce.reduce(1) {
-//    $0 * $1
+//
+//let newPlayers = [
+//    Player.init(name: "xj", score: 30),
+//    Player.init(name: "dcj", score: 20),
+//    Player.init(name: "tp", score: 59)
+//]
+//
+////let sortedPlayers = newPlayers.sorted { (item0, item1) -> Bool in
+////    return item0.score < item1.score
+////}
+//let sortedPlayers = newPlayers.sorted() {
+//    $0.score < $1.score
 //}
-//    numberReduce.reduce(1) { (result, item) -> Int in
-//    return result * item
+//print(sortedPlayers)
+
+/// forEach
+/// forEach does not return anything
+let birds = ["Owl", "Crane"]
+//birds.forEach { (item) in
+//    print(item)
 //}
+for item in birds {
+    print(item)
+}
 
-print(numberProduct)
+func findFirstNegativeNumber(nums: [Int]) -> Int? {
+    
+//    for item in nums {
+//        if item < 0 {
+//            return item
+//        }
+//    }
+//    return nil
+    var result: Int?
+    nums.forEach { (item) in
+        if item < 0 {
+            if result == nil {
+                result = item
 
-
+            }
+            
+        }
+    }
+    return result
+    
+}
+let numbers = [82, 5, -25, 10, -99]
+if let result = findFirstNegativeNumber(nums: numbers) {
+    print(result)
+}
 
 
 
