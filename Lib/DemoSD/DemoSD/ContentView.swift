a5e120bfe137ce102ce2e6cf845d70927f6160c4
dengchaojie <dengchaojiedcj@163.com>
2019-11-24 11:00:20 +0800

    new

---
+++ Lib/DemoSD/DemoSD/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DemoSD
+//
+//  Created by chao jie deng on 2019/11/24.
+//  Copyright © 2019 chao jie deng. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
