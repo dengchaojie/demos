8be5721dc4acf8f4b586445059cdcc459dc55e86
dengchaojie <dengchaojiedcj@163.com>
2019-11-29 22:42:19 +0800

    new

---
+++ Swift/SonarSwiftRule/SonarSwiftRule/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SonarSwiftRule
+//
+//  Created by chao jie deng on 2019/11/29.
+//  Copyright © 2019 deng chao jie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
