913cc1cefa1de42059f787eac95bd4b4d959a744
dengchaojie <dengchaojiedcj@163.com>
2020-01-10 13:38:46 +0800

    new

--- Swift/Tips.playground/Contents.swift
+++ Swift/Tips.playground/Contents.swift
@@ -1,8 +1,10 @@
 import UIKit
+//有1个遗留问题
+//https://mp.weixin.qq.com/s/GZl7Cbf1ZG83UFtInEFnyQ
 
 var str = "Hello, playground"
 
-
+//
 // no bad
 func updateWithView(view: UIView) {
 
@@ -16,12 +18,12 @@ func updateWith(view: UIView) {
 updateWith(view: UIView())
 
 func didSelectAtIndex(index: Int) {
-    
+
 }
 didSelectAtIndex(index: 2)
 
 func didSelect(at index: Int) {
-    
+
 }
 didSelect(at: 3)
 
@@ -61,6 +63,9 @@ protocol View {
     associatedtype Model
     func update(model: Model)
 }
+enum SomeError: Error {
+    case rejectError
+}
 
 enum State<Value> {
     case pending
@@ -75,10 +80,101 @@ enum State<Value> {
     }
     
 }
-
+// 问题：怎么调用下面的方法？
 extension State {
     func toString() {
         print("kdkd")
     }
 }
+var sta = State<Int>.pending
+sta.toString()
+sta.update(to: .fullfill(value: 2))
+sta.update(to: .reject(reson: SomeError.rejectError))
+
+//14. 使用 `` 来定义和关键字重名的方法和属性
+//let `default` = "dcj"
+func `default`() {
+    print("default")
+}
+`default`()
+
+//15. Strong-Weak Dance 很简单
+//func test() { [weak self] in
+//
+//    guard let self = self else {
+//        return
+//    }
+////     self is strong without retain cycle
+//}
+
+//18. 使用闭包做初始化
+let someView: UIView = {
+    let view = UIView()
+    view.backgroundColor = UIColor.blue
+    return view
+    
+}()
+
+// lazy
+struct Person {
+    let name: String
+    let age: Int
+    lazy var per = NSObject()
+}
+
+
+// 问题：where怎么用
+//<Value> where Value == String
+// https://www.avanderlee.com/swift/where-using-swift/
+extension Array where Element == Int {
+    func printAverageAge() -> Void {
+        let total = reduce(0, +)
+        let average = total / count
+        print("average age is \(average)")
+        
+        
+    }
+    
+}
+
+let arr = [20,30, 40, 40,60]
+arr.printAverageAge()
+arr.compactMap { (some) -> Void in
+    print(some)
+}
+let names = ["Henk", "John", "Jack"]
+let firstJName = names.first { (name) -> Bool in
+    return name.first == "J"
+}
+print(firstJName) // Returns John
+
+
+let fruits = ["Banana", "Apple", "Kiwi"]
+let isContainsBanna = fruits.contains { (fruit) -> Bool in
+    return fruit == "Banana"
+}
+
+
+//extension String {
+//    init(collection: T) where T.Element == String {
+//        self = collection.joined(separator: ",")
+//    }
+//}
+
+
+
+protocol SomeProtocol {
+    
+    // better
+    func updateWithView(_ view:UIView)
+    
+}
+
+struct SomeStruct: SomeProtocol {
+    
+    func updateWithView(_ view: UIView) {
+        print("ldl")
+    }
+}
+
 
